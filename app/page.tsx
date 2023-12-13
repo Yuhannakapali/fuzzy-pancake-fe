1d4ec0e298c7069196014c661ddc20bd56770076
upesh chalise <upeshchalise@gmail.com>
2023-12-13 15:37:55 +0545

    made login request and protected routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import Image from 'next/image'
+import Link from "next/link";
 
 export default function Home() {
   return (
-   <h1>This is the start</h1>
-  )
+    <>
+      <h1>This is the start</h1>
+      <Link href="/login">
+      <button className="border-2 border-blue-700 rounded bg-blue-200">Login</button>
+      </Link>
+    </>
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // domains: ['http://localhost:8000']
+}
 
 module.exports = nextConfig
